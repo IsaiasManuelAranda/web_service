3f1e5f374366af30308a14482c3466b144c188e6
IsaiasArandaLeal <1717110610@utectulancingo.edu.mx>
2019-03-22 23:27:07 -0600

    Creación de la vista de la actividad principal

--- app/src/main/java/com/example/webservice/MainActivity.java
+++ app/src/main/java/com/example/webservice/MainActivity.java
@@ -3,6 +3,19 @@ package com.example.webservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
